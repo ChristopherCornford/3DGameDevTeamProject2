56258547f26c0d607109a6451c253392b3dc28b4
ChristopherCornford <ccornford02906@gmail.com>
2018-04-09 15:18:22 -0400

    RayCasting

---
+++ Assets/Scripts/HideWalls.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideWalls : MonoBehaviour {
+
+
+	public Transform player;
+
+	public List<Transform> hiddenObjects;
+
+	public LayerMask layerMask;
+	public Ray ray;
+	public Color visableColor;
+	public Color hiddenColor;
+
+	// Use this for initialization
+	void Start () {
+		hiddenObjects = new List<Transform>();
+		ray = Camera.main.ScreenPointToRay (transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShootRay ();
+	}
+	void ShootRay() {
+		Vector3 direction = player.position - transform.position;
+		float distance = direction.magnitude;
+		Debug.DrawRay (transform.position, direction, Color.red);
+		RaycastHit[] hits = Physics.RaycastAll (transform.position, direction, distance, layerMask);
+
+		for (int i = 0; i < hits.Length; i++) {
+			Transform currentHit = hits [i].transform;
+
+			if (!hiddenObjects.Contains (currentHit)) {
+				hiddenObjects.Add (currentHit);
+				currentHit.GetComponent<MeshRenderer> ().material.color = hiddenColor;
+			}
+		}
+		for (int i = 0; i < hiddenObjects.Count; i++) {
+
+			bool isHit = false;
+
+			for (int j = 0; j < hits.Length; j++) {
+
+				if (hits[j].transform == hiddenObjects[i]) {
+					isHit = true;
+					break;
+				}
+			}
+			if (!isHit) {
+				Transform wasHidden = hiddenObjects [i];
+				wasHidden.GetComponent<MeshRenderer> ().material.color = visableColor;
+				hiddenObjects.RemoveAt (i);
+				i--;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	[Header("Movement")]
+	public float playerSpeed;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Move ();
+	}
+
+	void Move() {
+
+		float horizontalMovement = Input.GetAxisRaw ("Horizontal");
+		float verticalMovement = Input.GetAxisRaw ("Vertical");
+
+		Vector3 move = new Vector3 (-verticalMovement, 0.0f, horizontalMovement);
+		rb.AddForce (move * playerSpeed);
+	}
+}
