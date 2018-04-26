ca11c14e16c38337d965b7d3589b0b3d52f9c8ac
ChristopherCornford <ccornford02906@gmail.com>
2018-04-26 17:03:00 -0400

    ItemCreation

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType {
+	shortSword, 
+	Dagger,
+	smallMeal,
+	bigMeal
+}
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class Items : ScriptableObject{
+
+	public new string name;
+	public ItemType itemType;
+	[Space]
+	public GameObject prefab;
+
+}
