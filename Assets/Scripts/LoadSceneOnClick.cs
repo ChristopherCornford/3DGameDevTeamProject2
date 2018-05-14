9c1fd5e02099e95748ea90beb14065aa0350c7f5
Christopher A Cornford <ccornford02906@gmail.com>
2018-05-14 14:36:26 -0400

    Start/Pause Screen

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+	public int sceneIndex;
+
+	public void LoadByIndex(int sceneIndex)
+	{
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
